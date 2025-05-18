557f55ac7e000c49cbfcd59e240cfae1ace04e6b
TunaX540 <imtuna540@gmail.com>
2025-05-18 17:36:04 +0700

    Done

--- lib/views/input_screen_ui.dart
+++ lib/views/input_screen_ui.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: unused_field, prefer_final_fields, sort_child_properties_last
+
 import 'package:flutter/material.dart';
+import 'package:flutter_car_installment_calculator/views/result_screen_ui.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class InputScreenUI extends StatefulWidget {
   const InputScreenUI({super.key});
@@ -8,6 +13,57 @@ class InputScreenUI extends StatefulWidget {
 }
 
 class _InputScreenUIState extends State<InputScreenUI> {
+  TextEditingController _carPriceCtrl = TextEditingController();
+  TextEditingController _interestCtrl = TextEditingController();
+
+  int _downCtrl = 10;
+  int _monthCtrl = 24;
+
+  _warningDialog(msg) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('คำเตือน'),
+        content: Text(msg),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('ตกลง')),
+        ],
+      ),
+    );
+  }
+
+  File? _imageFile;
+
+  //สร้างเมธอดเปิดกล้อง
+  _openCamera() async {
+    final pickerImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+    );
+
+    if (pickerImage == null) return;
+
+    setState(() {
+      _imageFile = File(pickerImage.path);
+    });
+  }
+
+  //สร้างเมธอดเปิดแกลอรี่
+  _openGallery() async {
+    final pickerImage = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickerImage == null) return;
+
+    setState(() {
+      _imageFile = File(pickerImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +74,307 @@ class _InputScreenUIState extends State<InputScreenUI> {
           style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 40,
+            right: 40,
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'คำนวณค่างวดรถ',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Column(
+                            children: [
+                              ListTile(
+                                leading: Icon(Icons.camera),
+                                title: Text('เปิดกล้อง'),
+                                onTap: () {
+                                  _openCamera();
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              ListTile(
+                                leading: Icon(Icons.photo),
+                                title: Text('เปิดแกลอรี่'),
+                                onTap: () {
+                                  _openGallery();
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    child: _imageFile == null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.asset(
+                              'assets/images/img02.png',
+                              width: 200,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        : ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.file(
+                              _imageFile!,
+                              width: 200,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'ราคารถ(บาท)',
+                  ),
+                ),
+                TextField(
+                  controller: _carPriceCtrl,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '0.00',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'จำนวนเงินดาวน์(%)',
+                  ),
+                ),
+                Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    Radio(
+                      onChanged: (value) => {
+                        setState(() {
+                          _downCtrl = value!;
+                        })
+                      },
+                      value: 10,
+                      groupValue: _downCtrl,
+                    ),
+                    Text('10'),
+                    Radio(
+                      onChanged: (value) => {
+                        setState(() {
+                          _downCtrl = value!;
+                        })
+                      },
+                      value: 15,
+                      groupValue: _downCtrl,
+                    ),
+                    Text('15'),
+                    Radio(
+                      onChanged: (value) => {
+                        setState(() {
+                          _downCtrl = value!;
+                        })
+                      },
+                      value: 20,
+                      groupValue: _downCtrl,
+                    ),
+                    Text('20'),
+                    Radio(
+                      onChanged: (value) => {
+                        setState(() {
+                          _downCtrl = value!;
+                        })
+                      },
+                      value: 25,
+                      groupValue: _downCtrl,
+                    ),
+                    Text('25'),
+                    Radio(
+                      onChanged: (value) => {
+                        setState(() {
+                          _downCtrl = value!;
+                        })
+                      },
+                      value: 30,
+                      groupValue: _downCtrl,
+                    ),
+                    Text('30'),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'ระยะเวลาผ่อน(เดือน)',
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                    ),
+                    child: DropdownButton<int>(
+                      value: _monthCtrl,
+                      isExpanded: true,
+                      underline: Container(),
+                      onChanged: (int? value) {
+                        setState(() {
+                          _monthCtrl = value!;
+                        });
+                      },
+                      items: <int>[
+                        24,
+                        36,
+                        48,
+                        60,
+                        72,
+                      ].map<DropdownMenuItem<int>>((int value) {
+                        return DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(
+                            '$value เดือน',
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'อัตราดอกเบี้ย(%/ปี)',
+                  ),
+                ),
+                TextField(
+                  controller: _interestCtrl,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '0.00',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_carPriceCtrl.text.isEmpty) {
+                      _warningDialog('กรุณากรอกราคารถ');
+                    } else if (_interestCtrl.text.isEmpty) {
+                      _warningDialog('กรุณากรอกอัตราดอกเบี้ย');
+                    } else {
+                      double carPrice = double.parse(_carPriceCtrl.text);
+                      double interest = double.parse(_interestCtrl.text);
+
+                      double balance = carPrice - (carPrice * _downCtrl / 100);
+                      double totalInterest =
+                          (balance * interest / 100 / 12) * _monthCtrl;
+                      double payPerMonth =
+                          (balance + totalInterest) / _monthCtrl;
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ResultScreenUI(
+                            carPrice: carPrice,
+                            payPerMonth: payPerMonth,
+                            month: _monthCtrl,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text(
+                    'คำนวณ',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    minimumSize: Size(
+                      double.infinity,
+                      55,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'ยกเลิก',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    minimumSize: Size(
+                      double.infinity,
+                      55,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/views/result_screen_ui.dart
+++ lib/views/result_screen_ui.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ResultScreenUI extends StatefulWidget {
-  const ResultScreenUI({super.key});
+  double? carPrice;
+  double? payPerMonth;
+  int? month;
 
+  ResultScreenUI({super.key, this.carPrice, this.payPerMonth, this.month});
   @override
   State<ResultScreenUI> createState() => _ResultScreenUIState();
 }
@@ -10,6 +14,48 @@ class ResultScreenUI extends StatefulWidget {
 class _ResultScreenUIState extends State<ResultScreenUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    NumberFormat fm = NumberFormat(
+      "#,###,##0.##",
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 183, 0, 255),
+        title: Text(
+          'Results',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('ผ่อนทั้งหมด'),
+                Text(
+                  '${widget.month}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                Text('เดือน'),
+              ],
+            ),
+            Text(
+              '${widget.payPerMonth}',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
+              ),
+            ),
+            Text('บาทต่อเดือน'),
+          ],
+        ),
+      ),
+    );
   }
 }
